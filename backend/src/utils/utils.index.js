ce864c976c88a911b9e7fa1b103992d3a31ff1c8
kuldeepchaudhary75 <choudharykuldeep5511@gmail.com>
2026-07-25 01:00:41 +0530

    error due to case senstive

--- backend/src/utils/utils.index.js
+++ backend/src/utils/utils.index.js
@@ -1,6 +1,6 @@
 import { ApiError } from "./ApiError.js";
-import { ApiResponse } from "./apiResponse.js";
-import { asyncHandler } from "./asyncHandler.js";
+import { ApiResponse } from "./ApiResponse.js";
+import { asyncHandler } from "./AsyncHandler.js";
 import { generateBranchName } from "./branchName.js";
 
 export { ApiError, ApiResponse, asyncHandler, generateBranchName };
